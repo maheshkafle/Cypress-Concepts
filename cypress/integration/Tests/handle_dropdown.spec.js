1e357f4049311fe56e28d4f542b135d0c2a847f9
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-02-16 23:08:11 +0400

    Verified different dropdown scenarios

--- cypress/integration/Tests/handle_dropdown.spec.js
+++ cypress/integration/Tests/handle_dropdown.spec.js
@@ -1,5 +1,6 @@
 describe('Handle Dropdown Test Suite', ()=> {
 
+    //Note: use .skip() method to skip it block if needed while debugging
     it('Verify dropdown handle', ()=> {
 
         cy.visit("https://www.orangehrm.com/orangehrm-30-day-trial/")
@@ -8,4 +9,31 @@ describe('Handle Dropdown Test Suite', ()=> {
         .should('have.value', 'Nepal')
 
     })
+
+    it('Verify Google search and click auto suggestions', ()=> {
+
+        cy.visit("https://www.google.com/")
+
+        cy.get("#SIvCob a:nth-child(2)")
+        .click()
+       
+        cy.get(".gLFyf.gsfi")
+        .type('Cypress')
+
+        cy.get(".G43f7e li span")
+        .contains("cypress tree").click()
+
+    })
+
+    it('Verify ecommerce search and click the product', ()=> {
+
+        cy.visit("http://automationpractice.com/index.php")
+
+        cy.get("#search_query_top")
+        .type('printed')
+
+        cy.get('.ac_results ul li').contains('Evening Dresses > ').click()
+       
+    })
+    
 })
