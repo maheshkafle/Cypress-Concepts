a444e092ecb9622a02c2993dce33b785cbf12599
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-02-17 22:48:18 +0400

    verify user can search the product

--- cypress/integration/Tests/automation_practice_test.spec.js
+++ cypress/integration/Tests/automation_practice_test.spec.js
@@ -3,19 +3,19 @@ describe('Automation Practice Ecommerce Site Test Suite', ()=> {
     beforeEach(()=>{
         cy.visit('http://automationpractice.com')
         cy.login('smarshqa@gmail.com', '@Nepal123')
-
     })
 
     it('Verify user can login', ()=> {
-
         cy.url().should('include', 'controller=my-account')
         cy.get('.myaccount-link-list a').should('have.length', 5)
-
-
     })
 
     it('Verify user can navigate back to home page after login', ()=> {
-
         cy.get('.icon-chevron-left').last().click()
+    })
+
+    it('Verify user can search product using search bar', ()=> {
+        cy.search("dress")
+        cy.get('.lighter').should('contains.text', 'dress')
     })
 })
